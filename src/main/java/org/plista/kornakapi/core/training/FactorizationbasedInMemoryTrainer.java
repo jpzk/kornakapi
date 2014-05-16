d4b90682154d89b53da9b545470ab87207ebd117
maxweule <mw@plista.com>
2014-05-16 11:20:07 +0200

    Prevented modelcalculation for domains with to few ratings(matrix singular error)

--- src/main/java/org/plista/kornakapi/core/training/FactorizationbasedInMemoryTrainer.java
+++ src/main/java/org/plista/kornakapi/core/training/FactorizationbasedInMemoryTrainer.java
@@ -40,7 +40,7 @@ public class FactorizationbasedInMemoryTrainer extends AbstractTrainer {
   @Override
   protected void doTrain(File targetFile, DataModel inmemoryData, int numProcessors) throws IOException {
     try {
-    	if(inmemoryData.getNumItems() !=0){
+    	if(inmemoryData.getNumItems() >5 && inmemoryData.getNumUsers() >10){
 	      ALSWRFactorizer factorizer = new ALSWRFactorizer(inmemoryData, conf.getNumberOfFeatures(), conf.getLambda(),
 	          conf.getNumberOfIterations(), conf.isUsesImplicitFeedback(), conf.getAlpha(), numProcessors);
 	      
