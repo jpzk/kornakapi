bee24efbec6e7eae17230bbf4e0f87c50c0fdf9e
maxweule <mw@plista.com>
2014-07-30 14:53:08 +0200

    logging

--- src/main/java/org/plista/kornakapi/web/servlets/DeleteCandidateServlet.java
+++ src/main/java/org/plista/kornakapi/web/servlets/DeleteCandidateServlet.java
@@ -16,20 +16,30 @@
 package org.plista.kornakapi.web.servlets;
 
 import org.plista.kornakapi.web.Parameters;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 
 /** servlet to delete items from a candidate set */
 public class DeleteCandidateServlet extends BaseServlet {
+	  private static final Logger log = LoggerFactory.getLogger(RecommendServlet.class);
 
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     String label = getParameter(request, Parameters.LABEL, true);
     long itemID = getParameterAsLong(request, Parameters.ITEM_ID, true);
-    this.storages().get(label).deleteCandidate(label, itemID);
+    try{
+        this.storages().get(label).deleteCandidate(label, itemID);
+    } catch(NullPointerException e){
+    	  if(log.isInfoEnabled()){
+    		  log.info("No Recommender found for domain {} and itemID", label, itemID );
+    	  }
+    }
   }
 }
