51a320dec9a87f968e7f03e96ef6a4f4e79ed898
maxweule <mw@plista.com>
2014-08-12 15:47:02 +0200

    Changed Parameter Range

--- src/main/java/org/plista/kornakapi/core/optimizer/FactorizationBasedInMemoryOptimizer.java
+++ src/main/java/org/plista/kornakapi/core/optimizer/FactorizationBasedInMemoryOptimizer.java
@@ -47,8 +47,8 @@ public class FactorizationBasedInMemoryOptimizer extends AbstractOptimizer{
 		testSets.add(data.testData(1));
 		testSets.add(data.testData(2));
 			
-		ArrayList<Double> alphas = new ArrayList<Double>(Arrays.asList(190.0, 210.0,230.0,250.0,270.0));
-		ArrayList<Double> lambdas = new ArrayList<Double>(Arrays.asList(1000.0, 1400.0, 1800.0,2200.0,2600.0,3000.0,3400.0, 3800.0, 4200.0, 4600.0));
+		ArrayList<Double> alphas = new ArrayList<Double>(Arrays.asList(190.0, 210.0,230.0,250.0,270.0,280.0,290.0,300.0,320.0,340.0,360.0));
+		ArrayList<Double> lambdas = new ArrayList<Double>(Arrays.asList(1800.0,1900.0,2000.0,2100.0,2200.0,2300.0,2400.0,2500.0));
 		ArrayList<Integer> features = new ArrayList<Integer>(Arrays.asList(5,1));
 		ArrayList<Integer> iterations = new ArrayList<Integer>(Arrays.asList(10));
 		
