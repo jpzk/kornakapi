205f4fd85de7ea8509e071cee2a1489b52537560
maxweule <mw@plista.com>
2014-08-13 10:20:07 +0200

    Changed Parameter Range

--- src/main/java/org/plista/kornakapi/core/training/ALSWRFactorizer.java
+++ src/main/java/org/plista/kornakapi/core/training/ALSWRFactorizer.java
@@ -201,7 +201,7 @@ public class ALSWRFactorizer extends AbstractFactorizer {
                 public void run() { 
                 
                 Vector userFeatures = implicitFeedbackSolver.solve(sparseUserRatingVector(userPrefs));
-                userFeatures = userFeatures.divide(Math.sqrt(userFeatures.getLengthSquared()));
+                //userFeatures = userFeatures.divide(Math.sqrt(userFeatures.getLengthSquared()));
                 features.setFeatureColumnInU(userIndex(userID), userFeatures);
 
                 }
@@ -251,7 +251,7 @@ public class ALSWRFactorizer extends AbstractFactorizer {
           	            @Override
           	            public void run() {
           	            Vector itemFeatures = implicitFeedbackSolver.solve(sparseItemRatingVector(itemPrefs));
-          	            itemFeatures = itemFeatures.divide(Math.sqrt(itemFeatures.getLengthSquared()));
+          	            //itemFeatures = itemFeatures.divide(Math.sqrt(itemFeatures.getLengthSquared()));
           	            features.setFeatureColumnInM(itemIndex(itemID), itemFeatures);
           	            }
           	          });
