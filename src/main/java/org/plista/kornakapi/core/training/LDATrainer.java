61d51ff428aeb0b7cfb86896dff2bd2bb47f0fab
maxweule <mw@plista.com>
2014-10-08 17:01:34 +0200

    TopicDistribution are now printed out

--- src/main/java/org/plista/kornakapi/core/training/LDATrainer.java
+++ src/main/java/org/plista/kornakapi/core/training/LDATrainer.java
@@ -18,9 +18,14 @@ package org.plista.kornakapi.core.training;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.utils.vectors.VectorDumper;
+import org.plista.kornakapi.core.config.LDARecommenderConfig;
 import org.plista.kornakapi.core.config.RecommenderConfig;
+
+import com.google.common.collect.Lists;
 
 public class LDATrainer extends AbstractTrainer{
 	
@@ -37,10 +42,64 @@ public class LDATrainer extends AbstractTrainer{
 		try {
 			new FromDirectoryVectorizer(conf).doTrain();
 			new LDATopicModeller(conf).doTrain();
+			printLocalTopicWordDistribution(conf,((LDARecommenderConfig)conf).getTopicsOutputPath(),((LDARecommenderConfig)conf).getTopicsOutputPath());
+			printLocalDocumentTopicDistribution(conf,((LDARecommenderConfig)conf).getLDADocTopicsPath(),((LDARecommenderConfig)conf).getLDADocTopicsPath());
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 	}
+	
+	public static void printLocalTopicWordDistribution(RecommenderConfig conf, String input, String output){
+	       List<String> argList = Lists.newLinkedList();
+	        argList.add("-i");
+	        argList.add(input);
+	        argList.add("-o");
+	        argList.add("/opt/kornakapi-model/lda/print/topics.txt");
+	        argList.add("--dictionaryType");
+	        argList.add("sequencefile");
+	        argList.add("-d");
+	        argList.add(((LDARecommenderConfig)conf).getTopicsDictionaryPath());
+	        argList.add("-sort");
+	        argList.add("true");
+	        argList.add("-vs");
+	        argList.add("20");
+
+	        String[] args = argList.toArray(new String[argList.size()]);
+	        try {
+				//LDAPrintTopics.main(args);
+				VectorDumper.main(args);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+	}
+	
+	public static void printLocalDocumentTopicDistribution(RecommenderConfig conf, String input, String output){
+	       List<String> argList = Lists.newLinkedList();
+	        argList.add("-i");
+	        argList.add(input);
+	        argList.add("-o");
+	        argList.add("/opt/kornakapi-model/lda/print/DocumentTopics.txt");
+	        argList.add("-sort");
+	        argList.add("true");
+	        argList.add("-vs");
+	        argList.add("20");
+	        argList.add("-p");
+	        argList.add("true");
+
+
+	        String[] args = argList.toArray(new String[argList.size()]);
+	        try {
+				//LDAPrintTopics.main(args);
+				VectorDumper.main(args);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+	}
+	
 }
