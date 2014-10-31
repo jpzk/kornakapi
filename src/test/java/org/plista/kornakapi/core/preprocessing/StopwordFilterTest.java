926f6b4af9efbc2af0b2b11380cb98b76a2d45d5
Jendrik Poloczek <jendrik.poloczek@madewithtea.com>
2014-10-31 09:26:53 +0100

    fixed test

--- src/test/java/org/plista/kornakapi/core/preprocessing/StopwordFilterTest.java
+++ src/test/java/org/plista/kornakapi/core/preprocessing/StopwordFilterTest.java
@@ -1,24 +1,27 @@
 package org.plista.kornakapi.core.preprocessing;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Set;
 
 import org.junit.Test;
+import org.plista.kornakapi.core.config.Configuration;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
 
 import junit.framework.TestCase;
 
 public class StopwordFilterTest extends TestCase {
 
 	@Test
-	public void testListRead() throws IOException {
-		StopwordFilter swf = new StopwordFilter();
-		Set<String> stopwords = swf.getStopwords();
-		assertTrue(stopwords.size() > 0);
-	}
-
-	@Test
 	public void testFilterText() throws IOException {
-		StopwordFilter swf = new StopwordFilter();
+		String path = "/etc/kornakapi/kornakapi.conf";
+		File configFile = new File(path);
+		System.out.print(configFile.canRead());
+		Configuration conf =Configuration.fromXML(Files.toString(configFile, Charsets.UTF_8));	
+		
+		StopwordFilter swf = new StopwordFilter(conf.getPreprocessingDataDirectory() + "kornakapi_sw_de.txt");
 		String output = swf.filterText("Das ist ein Test");
 		assertEquals(output, "test");
 	}
